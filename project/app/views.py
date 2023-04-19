f2e032a23c2641f26634952cd4bc29e6848f6b90
Tais8675 <nascimentotaais@gmail.com>
2023-04-19 18:28:50 -0300

    commit 3

---
+++ project/app/views.py
@@ -0,0 +1,22 @@
+from audioop import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .models import App, Lista
+
+# Create your views here.
+
+
+
+def index(request):
+    return render(request, "app/index.html", {
+        "app": App.objects.all()
+    })
+
+def app(request, app_id):
+    compra = App.objects.get(id=app_id)
+    print(compra)
+    return render(request, "app/pag.html", {
+        "app": compra
+        
+      
+    })
